d9396db941ddcbd0cc78a75364adb0df77aff2fd
Nader Helmy <creator.nader@gmail.com>
2026-07-24 17:39:37 -0500

    fix: update vulnerable web dependencies

--- web/worker/test/env.d.ts
+++ web/worker/test/env.d.ts
@@ -1,5 +1,13 @@
-import type { Env } from "../src/index";
+import type { Env as WorkerEnv } from "../src/index";
 
 declare module "cloudflare:test" {
-  interface ProvidedEnv extends Env {}
+  interface ProvidedEnv extends WorkerEnv {}
 }
+
+declare global {
+  namespace Cloudflare {
+    interface Env extends WorkerEnv {}
+  }
+}
+
+export {};
